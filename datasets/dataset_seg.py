d732a01453b66346ac3761b92c35a44a0fd1b0a1
buvanasri-tj <tjbuvanasri2002@gmail.com>
2025-11-20 16:07:36 +0530

    Update dataset_seg.py

--- datasets/dataset_seg.py
+++ datasets/dataset_seg.py
@@ -1,9 +1,9 @@
 import os
 import glob
+import torch
 from PIL import Image
 from torch.utils.data import Dataset
 import torchvision.transforms as T
-import torch
 
 
 class SegmentationDataset(Dataset):
@@ -13,45 +13,46 @@ class SegmentationDataset(Dataset):
         self.image_size = image_size
         self.augment = augment
 
-        # Directories for channels
-        self.green_img = os.path.join(root, "green", split, "images")
-        self.red_img = os.path.join(root, "red", split, "images")
+        # channel folders
+        self.green_img  = os.path.join(root, "green",  split, "images")
+        self.red_img    = os.path.join(root, "red",    split, "images")
         self.yellow_img = os.path.join(root, "yellow", split, "images")
-        self.green_mask = os.path.join(root, "green", split, "masks")
+        self.mask_dir   = os.path.join(root, "green",  split, "masks")
 
-        # Base on green image folder
-        self.ids = sorted([os.path.splitext(os.path.basename(f))[0]
-                           for f in glob.glob(os.path.join(self.green_img, "*.png"))])
+        self.ids = sorted([
+            os.path.splitext(os.path.basename(f))[0]
+            for f in glob.glob(os.path.join(self.green_img, "*.png"))
+        ])
 
-        self.img_transform = T.Compose([
+        self.img_tf = T.Compose([
             T.Resize((image_size, image_size)),
             T.ToTensor()
         ])
 
-        self.mask_transform = T.Compose([
+        self.mask_tf = T.Compose([
             T.Resize((image_size, image_size), interpolation=T.InterpolationMode.NEAREST),
             T.ToTensor()
         ])
 
+    def _load_gray(self, path):
+        return Image.open(path).convert("L")
+
     def __len__(self):
         return len(self.ids)
-
-    def _load(self, path):
-        return Image.open(path).convert("L")
 
     def __getitem__(self, idx):
         id_ = self.ids[idx]
 
-        g = self._load(os.path.join(self.green_img, f"{id_}.png"))
-        r = self._load(os.path.join(self.red_img, f"{id_}.png"))
-        y = self._load(os.path.join(self.yellow_img, f"{id_}.png"))
-        m = self._load(os.path.join(self.green_mask, f"{id_}.png"))
+        g = self._load_gray(os.path.join(self.green_img,  f"{id_}.png"))
+        r = self._load_gray(os.path.join(self.red_img,    f"{id_}.png"))
+        y = self._load_gray(os.path.join(self.yellow_img, f"{id_}.png"))
+        m = self._load_gray(os.path.join(self.mask_dir,   f"{id_}.png"))
 
-        g = self.img_transform(g)
-        r = self.img_transform(r)
-        y = self.img_transform(y)
-        m = self.mask_transform(m)
+        g = self.img_tf(g)
+        r = self.img_tf(r)
+        y = self.img_tf(y)
+        m = self.mask_tf(m)
 
-        rgb = torch.cat([g, r, y], dim=0)
+        img = torch.cat([g, r, y], dim=0)
 
-        return rgb, m
+        return img, m
