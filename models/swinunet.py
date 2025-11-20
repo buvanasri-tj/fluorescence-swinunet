74e4a8e89e1fecd06288fdeb76d4ad48ed38a66f
buvanasri-tj <tjbuvanasri2002@gmail.com>
2025-11-20 16:10:23 +0530

    Update swinunet.py

--- models/swinunet.py
+++ models/swinunet.py
@@ -1,4 +1,18 @@
-# models/swinunet.py
+import torch
+import torch.nn as nn
 from networks.swin_transformer_unet_skip_expand_decoder_sys import SwinTransformerSys
-def get_swinunet(num_classes=1, **kwargs):
-    return SwinTransformerSys(num_classes=num_classes, **kwargs)
+
+
+class SwinUNet(nn.Module):
+    def __init__(self, in_channels=3, num_classes=2):
+        super().__init__()
+
+        self.backbone = SwinTransformerSys(
+            img_size=256,
+            patch_size=4,
+            in_chans=in_channels,
+            num_classes=num_classes,
+        )
+
+    def forward(self, x):
+        return self.backbone(x)
