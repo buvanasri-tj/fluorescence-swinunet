79fca03c72358154573cf764d9f12e0f52d622c6
buvanasri-tj <tjbuvanasri2002@gmail.com>
2025-11-20 16:19:59 +0530

    Update data_utils.py

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,9 +1,22 @@
-import glob
+import os
+from PIL import Image
+import numpy as np
 
-def create_list_file(images_dir, masks_dir, output_txt):
-    images = sorted(glob.glob(images_dir + "/*.png"))
-    masks  = sorted(glob.glob(masks_dir + "/*.png"))
 
-    with open(output_txt, "w") as f:
-        for img, mask in zip(images, masks):
-            f.write(f"{img} {mask}\n")
+def load_gray(path):
+    return Image.open(path).convert("L")
+
+
+def normalize_img(img):
+    """
+    img: numpy array
+    returns: normalized [0,1] float32
+    """
+    img = np.array(img).astype("float32")
+    img = (img - img.min()) / (img.max() - img.min() + 1e-6)
+    return img
+
+
+def ensure_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
