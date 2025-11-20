bb898cfaf6d21e24e31398aaf9fac17b5db786d2
buvanasri-tj <tjbuvanasri2002@gmail.com>
2025-11-20 11:32:16 +0530

    Implement YOLO-style dataset loader for detection
    Refactored dataset loading to create a YOLO-style dataset class for bounding-box detection.

--- datasets/dataset_detect.py
+++ datasets/dataset_detect.py
@@ -1,24 +1,31 @@
-# datasets/dataset_detect.py
-# Convert segmentation masks to YOLO-style bbox labels (x_center y_center w h, normalized)
-
+# Simple YOLO-style dataset loader for bounding-box detection
 import os
-import cv2
-import numpy as np
+import torch
+from torch.utils.data import Dataset
+from PIL import Image
 
-def masks_to_bboxes(mask_path):
-    mask = cv2.imread(mask_path, 0)
-    _, thr = cv2.threshold(mask, 127, 255, cv2.THRESH_BINARY)
-    contours, _ = cv2.findContours(thr, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    boxes = []
-    h,w = mask.shape
-    for c in contours:
-        x,y,ww,hh = cv2.boundingRect(c)
-        x_center = (x + ww/2)/w
-        y_center = (y + hh/2)/h
-        boxes.append((0, x_center, y_center, ww/w, hh/h))  # class 0
-    return boxes
+class DetectionDataset(Dataset):
+    def __init__(self, list_file, transform=None):
+        self.items = [x.strip() for x in open(list_file)]
+        self.transform = transform
 
-def save_yolo_label(label_path, boxes):
-    with open(label_path, 'w') as f:
-        for b in boxes:
-            f.write(" ".join(map(str, b)) + "\n")
+    def __len__(self):
+        return len(self.items)
+
+    def __getitem__(self, idx):
+        img_path = self.items[idx]
+        label_path = img_path.replace("images", "labels").replace(".png", ".txt")
+
+        img = Image.open(img_path).convert("RGB")
+
+        boxes = []
+        if os.path.exists(label_path):
+            with open(label_path) as f:
+                for line in f:
+                    cls, x, y, w, h = map(float, line.split())
+                    boxes.append([cls, x, y, w, h])
+
+        if self.transform:
+            img = self.transform(img)
+
+        return img, torch.tensor(boxes), img_path
