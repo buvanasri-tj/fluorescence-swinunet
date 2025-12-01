f62a96cfce2951cf92e46cfecdb85ab2cd10be20
buvanasri-tj <tjbuvanasri2002@gmail.com>
2025-12-01 11:33:18 +0530

    Refactor SegmentationDataset to load images and masks

--- datasets/dataset_seg.py
+++ datasets/dataset_seg.py
@@ -1,4 +1,3 @@
-# datasets/dataset_seg.py
 import os
 from PIL import Image
 from torch.utils.data import Dataset
@@ -7,32 +6,23 @@ import torchvision.transforms as T
 
 class SegmentationDataset(Dataset):
     """
-    Dataset that reads:
-      root/train/images/*.png
-      root/train/masks/*.png
-    OR
-      root/val/images/*.png
-      root/val/masks/*.png
+    Automatically loads ALL images+masks from:
+    data/train/images/
+    data/train/masks/
+    data/val/images/
+    data/val/masks/
     """
 
     def __init__(self, root_dir, split="train", image_size=256):
-        self.root_dir = root_dir
-        self.split = split
-        self.img_dir = os.path.join(root_dir, split, "images")
-        self.mask_dir = os.path.join(root_dir, split, "masks")
+        self.root = os.path.join(root_dir, split)
+        self.img_dir = os.path.join(self.root, "images")
+        self.mask_dir = os.path.join(self.root, "masks")
 
-        self.image_size = image_size
+        self.files = sorted(os.listdir(self.img_dir))
 
-        self.images = sorted([
-            f for f in os.listdir(self.img_dir)
-            if f.endswith(".png")
-        ])
-
-        # transforms
         self.transform_img = T.Compose([
             T.Resize((image_size, image_size)),
             T.ToTensor(),
-            T.Normalize(mean=[0.5], std=[0.5]),
         ])
 
         self.transform_mask = T.Compose([
@@ -40,24 +30,19 @@ class SegmentationDataset(Dataset):
             T.ToTensor(),
         ])
 
-        print(f"[INFO] Loaded {len(self.images)} samples for split '{split}' from {root_dir}.")
-
     def __len__(self):
-        return len(self.images)
+        return len(self.files)
 
     def __getitem__(self, idx):
-        img_name = self.images[idx]
+        fname = self.files[idx]
+        img_path = os.path.join(self.img_dir, fname)
+        mask_path = os.path.join(self.mask_dir, fname)
 
-        img_path = os.path.join(self.img_dir, img_name)
-        mask_path = os.path.join(self.mask_dir, img_name)
-
-        img = Image.open(img_path).convert("L")
+        img = Image.open(img_path).convert("RGB")
         mask = Image.open(mask_path).convert("L")
 
         img = self.transform_img(img)
         mask = self.transform_mask(mask)
-
-        # convert {0,255} → {0,1}
         mask = (mask > 0.5).float()
 
         return img, mask
